21a17cd6aacf3e17cb9f70764d9c6693eec0db93
Michael23 <michaelzhou23@hotmail.com>
2020-12-28 16:34:39 +0800

    This is the first commit of the cachematrix.R file

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,36 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inv <- NULL
+      set <- function(y){
+            x <<- y
+            inv <<- NULL ##This is to make sure that if the matrix is changed, the cached inverse also needs to be reset
+      }
+      get <- function() x
+      
+      setInverse <- function(inverse) inv <<- inverse
+      getInverse <- function() inv
+      
+      list(set = set, get = get,
+           setInverse = setInverse,
+           getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
+      inv <- x$getInverse()
+      if(!is.null(inv)){
+            message("Getting cached inverse")
+            return(inv)
+      }
+      
+      data <- x$get()
+      inv <- solve(data)
+      x$setInverse(inv)
+      
+      inv
+      
 }
